2b9116ff7fe14a7b9a107b6a744792e30af64fe2
Brigette Motlatjo Setlago <148428651+brigettesetlago@users.noreply.github.com>
2024-02-01 09:28:54 +0200

    HackerRank4

---
+++ src/main/java/com/mycompany/solution4/Solution4.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solution4;
+
+/**
+ *
+ * @author Brigette Setlago
+ */
+      import java.util.Scanner;
+public class Solution4 {
+
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                //Complete this line
+            
+            System.out.printf("%-15s%03d%n",s1,x);
+
+            }
+     System.out.println("================================");
+}
+}
+
+    
+
